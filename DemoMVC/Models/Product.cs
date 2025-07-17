581dc2aeb183f6cd2b04be8c018be657fc6cad59
Mangat Singh <Mangat Singh@LAPTOP-J6E2UKQU>
2025-07-17 18:10:31 +0530

    Add project files.

---
+++ DemoMVC/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(0.01, 10000)]
+        public decimal Price { get; set; }
+
+        [Range(0, 1000)]
+        public int Quantity { get; set; }
+    }
+}
